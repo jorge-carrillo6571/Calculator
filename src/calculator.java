4a4df76b8a42e416beda16ded72b8909d6b94e65
Jorge <jorgecarrillo6571@gmail.com>
2025-05-14 13:45:18 -0700

    A: created calc class

---
+++ src/calculator.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class calculator implements ActionListener {
+    JFrame frame; 
+    JTextField textField; 
+    JButton[] numberButtons = new JButton[10]; 
+    JButton[] functioButtons = new JButton[8]; 
+    
+    calculator(){
+
+    }
+    public static void main (String [] args){
+        calculator calc = new calculator(); 
+
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+}
